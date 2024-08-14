edd85ee0cc0a8843927f5ce8f60c4344804e24f7
ebrahimmuhammed <ebrahimmuhammed107@gmail.com>
2024-08-14 11:44:59 +0530

    Add Create view and update build output

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -14,6 +14,32 @@ namespace MESS.Controllers
         {
             _context=context;
         }
+
+        public IActionResult Create()
+        {
+            return View();  
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Create(User user)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Add(user);
+                _context.SaveChanges();
+                
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+
+
+        }
+
+
+
 public IActionResult Index()
     {
         var users = _context.Users.ToList();
--- obj/Debug/net8.0/mess.AssemblyInfo.cs
+++ obj/Debug/net8.0/mess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4135437782490a6a3be390aa70c513a3b630ab6")]
 [assembly: System.Reflection.AssemblyProductAttribute("mess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
